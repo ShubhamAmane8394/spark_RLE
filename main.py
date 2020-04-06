2fe73297bf6af797739a68cdaabcc54fe14c4c0c
shubhamamane <samane@fpg-ingauge.con>
2020-04-06 21:07:59 +0530

    Initial Comment

--- main.py
+++ main.py
@@ -2,24 +2,45 @@ from spark_context import spark_contexts
 from itertools import groupby
 import re
 import sys
+from pyspark.serializers import PickleSerializer, AutoBatchedSerializer
+
 
 
 sparkcontext = spark_contexts().get_spark_context()
+
+def _to_java_object_rdd(rdd):
+    """ Return a JavaRDD of Object by unpickling
+    It will convert each Python object into Java object by Pyrolite, whenever the
+    RDD is serialized in batch or not.
+    """
+    rdd = rdd._reserialize(AutoBatchedSerializer(PickleSerializer()))
+    return rdd.ctx._jvm.org.apache.spark.mllib.api.python.SerDe.pythonToJava(rdd._jrdd, True)
+
+def get_size(file):
+    JavaObj = _to_java_object_rdd(file)
+    nbytes = sparkcontext._jvm.org.apache.spark.util.SizeEstimator.estimate(JavaObj)
+    return nbytes
 
 def rle_encoder(input, output):
 
     # Read the text file
     input_file = sparkcontext.textFile("file://{}".format(input))
-
+    input_file_size = get_size(input_file)
+    print(input_file_size)
     # Iterate through the each of the line and encode the line. Put this encoding logic into EncoderAlgoImplementation file
     read_file = input_file.map(lambda x: x if bool(re.search(r'\d', x)) == True else \
         ''.join([str(len(list(v))) + k for k, v in groupby(x)]))
-
+    output_file_size  = get_size(read_file)
+    print(output_file_size)
     # check for the diff in sizes of original data and latest encoded data
 
+    if input_file_size > output_file_size:
+        print(True)
+    else:
+        read_file.coalesce(1).saveAsTextFile("file:///{}".format(output))
 
     # If both are same discard it, else write the encoded data to to output folder
-    read_file.coalesce(1).saveAsTextFile("file:///{}".format(output))
+
     return read_file
 
 
--- start_script.py
+++
@@ -1,7 +0,0 @@
-import  os
-import sys
-os.path.dirname(os.path.realpath(__file__))
-file = "main.py"
-cmd = "spark-submit --master local[2] /"+file
-
-os.command(cmd)
